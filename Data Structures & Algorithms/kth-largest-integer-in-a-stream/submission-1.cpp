9210ef5a76d2056b1c225f754dca1552e4dead19
born2kill09 <guptarayirth@gmail.com>
2026-07-12 00:29:49 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cpp
@@ -0,0 +1,24 @@
+class KthLargest {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
+public:
+    KthLargest(int k, vector<int>& nums) {
+         this->k = k;
+        for(int it: nums)
+        {
+            pq.push(it);
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+        }}
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()> k)
+        {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
