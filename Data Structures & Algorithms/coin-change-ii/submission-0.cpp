029ff2271ce476a25b4450d39b7ca10bb69ef50c
born2kill09 <guptarayirth@gmail.com>
2026-07-16 20:16:19 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1; // Base case: 1 way to make amount 0 (using no coins)
+        
+        // For each coin, update how many ways we can reach every amount 'i'
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
