73ebc89470a7dfa8ea7de121f46024d0da634bcd
born2kill09 <guptarayirth@gmail.com>
2026-07-28 01:02:11 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int sum =0;
+        int count =0;
+        int i =0; 
+        int n = nums.size() - 1;
+        while(sum <= n)
+        {
+            int m =0;
+          for(int j = i; j < i+ sum; j++ )
+          {
+            m = max(m ,j + nums[j]);
+
+          }
+          i = sum + 1;
+          sum=  m;
+          count++;
+        }
+        return count;
+    }
+};
